e6b4e4a536c0c69cf0cae2dd1ad85cb555bf020e
Reinheal <dimabologov@gmail.com>
2018-08-20 09:57:59 +0300

    dao less17

---
+++ src/main/java/com/academy/lesson17/dao/OperatorNumbers.java
@@ -0,0 +1,24 @@
+package com.academy.lesson17.dao;
+
+import java.util.Map;
+import java.util.Set;
+
+public class OperatorNumbers {
+    private Map<String, Long> operatorNumbers;
+
+    public OperatorNumbers(Map<String, Long> operatorNumbers) {
+        this.operatorNumbers = operatorNumbers;
+    }
+    public boolean exists(String number, Long operator_id){
+        return true;
+    }
+    public Set<String> getAllByNumber(Long operator_id){
+        return null;
+    }
+    public boolean add(String number, Long operator_id){
+        return true;
+    }
+    public boolean remove(String number){
+        return true;
+    }
+}
